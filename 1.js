c469827b63b6c5f628bd506614d338beec0e1fd2
nguyenhuuluan14099 <80726728+nguyenhuuluan14099@users.noreply.github.com>
2021-07-22 22:02:42 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,21 @@
+﻿var nut = document.querySelectorAll(".button .btn");
+var khoi = document.querySelector(".khoi");
+console.log(khoi);
+for(var i = 0; i< nut.length;i++)
+{
+    nut[i].addEventListener("click",function(){
+        var show = this;
+        for(var vitri = 0;show = show.previousElementSibling;vitri++)
+        {
+            //console.log("ban da click"+vitri);
+        }
+        /* khoi.classList.remove("so"+vitri);   */
+        /* khoi[vitri].classList.add("so"+vitri); */
+        for(var a = 0;a<nut.length;a++)
+        {
+            khoi.classList.remove("so"+a);
+        }
+        khoi.classList.add("so"+vitri);
+
+    })
+}
